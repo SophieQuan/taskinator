bc962d26514776150f1183fa477e2362ad25e6a6
SophieQuan <sophiequan7@gmail.com>
2022-01-16 16:50:50 -0500

    add form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEL = document.querySelector("#tasks-to-do");
 
-var createTaskHander = function(){
+var createTaskHander = function(e){
+    e.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task";
     taskToDoEL.appendChild (listItemEl);
+    console.log(e);
 };
 
-buttonEl.addEventListener("click", createTaskHander);
+formEl.addEventListener("submit", createTaskHander);
 
